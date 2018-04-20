5115832f8de75adb20b7c94517c3581661c6bff8
zhangqi <13525505765@163.com>
2018-04-20 15:39:27 +0800

    修复计时器最后一页滚动过快的问题

--- Example/ZQCycleView/ZQViewController.m
+++ Example/ZQCycleView/ZQViewController.m
@@ -18,16 +18,7 @@
 {
     [super viewDidLoad];
 	
-    ZQCycleView *cycleView = [[ZQCycleView alloc] initWithframe:CGRectMake(20, 20, [UIScreen mainScreen].bounds.size.width - 40, 200) imageUrls:@[
-                                                                                           @"XXX.jpg",
-                                                                                           @"http://b.hiphotos.baidu.com/zhidao/pic/item/4b90f603738da9770889666fb151f8198718e3d4.jpg",
-                                                                                           @"http://g.hiphotos.baidu.com/zhidao/pic/item/f2deb48f8c5494ee4e84ef5d2cf5e0fe98257ed4.jpg",
-                                                                                           @"http://d.hiphotos.baidu.com/zhidao/pic/item/9922720e0cf3d7ca104edf32f31fbe096b63a93e.jpg",
-                                                                                           @"http://b.hiphotos.baidu.com/zhidao/pic/item/4b90f603738da9770889666fb151f8198718e3d4.jpg",
-                                                                                           
-                                                                                           @"http://g.hiphotos.baidu.com/zhidao/pic/item/f2deb48f8c5494ee4e84ef5d2cf5e0fe98257ed4.jpg"
-                                                                                           ]
-                                                       delegate:self
+    ZQCycleView *cycleView = [[ZQCycleView alloc] initWithframe:CGRectMake(20, 20, [UIScreen mainScreen].bounds.size.width - 40, 200) imageUrls:@[@"http://b.hiphotos.baidu.com/zhidao/pic/item/4b90f603738da9770889666fb151f8198718e3d4.jpg", @"http://g.hiphotos.baidu.com/zhidao/pic/item/f2deb48f8c5494ee4e84ef5d2cf5e0fe98257ed4.jpg", @"http://d.hiphotos.baidu.com/zhidao/pic/item/9922720e0cf3d7ca104edf32f31fbe096b63a93e.jpg", @"http://b.hiphotos.baidu.com/zhidao/pic/item/4b90f603738da9770889666fb151f8198718e3d4.jpg", @"http://g.hiphotos.baidu.com/zhidao/pic/item/f2deb48f8c5494ee4e84ef5d2cf5e0fe98257ed4.jpg"] delegate:self
                                          selectPageControlColor:[UIColor whiteColor] pageControlAliment: PageControlAlimentRight];
     
     [self.view addSubview:cycleView];
